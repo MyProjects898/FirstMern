0f3459651725a0d0f843208fbc1a48f692a4dda2
sxena <s_xena@hotmail.com>
2020-04-24 15:22:12 -0700

    full on MERN app

---
+++ src/components/map.component.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+// var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+// mapboxgl.accessToken = 'pk.eyJ1IjoiaGV5YWJpcmQiLCJhIjoiY2s5ZWl0c3M0MDJzdDNnbzE2dXB5bDRhdSJ9.bNbukgXKDz5ZbTc9gQ4-bQ';
+// var map = new mapboxgl.Map({
+// container: 'YOUR_CONTAINER_ELEMENT_ID',
+// style: 'mapbox://styles/mapbox/streets-v11'
+// });
+
+export default class Map extends Component {
+    render() {
+        return (
+            <div>
+                API insert
+            </div>
+        )
+    }
+}
