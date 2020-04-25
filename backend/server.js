c3ba09916a2397a6756b61f71d41898a12de9a8c
sxena <s_xena@hotmail.com>
2020-04-24 19:30:24 -0700

    changed port to 3000

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,11 @@ const app = express();
 // });
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+
 
 app.use(cors());
 app.use(express.json());
